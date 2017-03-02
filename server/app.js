1ea1964cc51a7bb4fe46888305fd6914649d5562
Michael Abood <Abood@Mikes-Macbook.local>
2017-03-02 08:14:17 -0600

    commit

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app     = express(),
-    server  = require('http').createServer(app);
+    server  = require('http').createServer(app),
+    path 	= require('path'); 
 
 
 
@@ -8,12 +9,18 @@ var express = require('express'),
   app.get('/', function(request, response){
     // request object is from the client,
     // the response is what were sending back
-    response.send('Hey I the server finally works')
+    response.send('Hey the server finally works')
+  })
+
+    app.get('/', function(request, response){
+    // request object is from the client,
+    // the response is what were sending back
+    response.send('Hey the server finally works')
   })
 
 
 
 // first argument is the port number
 server.listen(5000, function(){
-  console.log('server is listening, but on what port, are you reading the code?')
+  console.log('server is listening on port 5000')
 })
